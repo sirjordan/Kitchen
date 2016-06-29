0c22d7b2f116809564ce151c6229fa577c9d4d90
Martin <sirjordan1988@gmail.com>
2016-06-29 11:26:32 +0300

    merge with the ui skeleton

--- Kitchen.Web/App_Start/BundleConfig.cs
+++ Kitchen.Web/App_Start/BundleConfig.cs
@@ -20,11 +20,9 @@ namespace Kitchen.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
