edf0cd8da2ff48074aff127842346f1a080a62e4
Martin <sirjordan1988@gmail.com>
2020-03-18 17:18:27 +0200

    Config

---
+++ Kitchen.Web/ConfigurationManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Kitchen.Web
+{
+    public static class ConfigurationManager
+    {
+        static ConfigurationManager()
+        {
+            Configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+        }
+
+        public static IConfiguration Configuration { get; }
+
+        public class Settings
+        {
+            public static bool Develop => bool.Parse(Configuration["Settings:Develop"]);
+        }
+    }
+}
