eec57cb5db54b599063b417a7ac9bb2d3f51674c
Martin <sirjordan1988@gmail.com>
2020-03-18 14:53:55 +0200

    Dish partial view

---
+++ Kitchen.Web/Models/DishViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Kitchen.Web.Models
+{
+    public class DishViewModel
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string Allergens { get; set; }
+    }
+}
